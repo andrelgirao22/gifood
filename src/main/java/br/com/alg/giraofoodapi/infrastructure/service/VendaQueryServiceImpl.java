bca31ad8512c3b0d15b535d96cc43ddebf1eac76
andre girao <andrelgirao29@gmail.com>
2022-02-02 22:24:58 -0300

    Desafio: adicionando os filtros na consulta de vendas diárias

--- src/main/java/br/com/alg/giraofoodapi/infrastructure/service/VendaQueryServiceImpl.java
+++ src/main/java/br/com/alg/giraofoodapi/infrastructure/service/VendaQueryServiceImpl.java
@@ -11,8 +11,10 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import javax.persistence.criteria.Predicate;
 
 @Repository
 public class VendaQueryServiceImpl implements VendaQueryService {
@@ -25,6 +27,7 @@ public class VendaQueryServiceImpl implements VendaQueryService {
         var builder = manager.getCriteriaBuilder();
         var query = builder.createQuery(VendaDiaria.class);
         var root = query.from(Pedido.class);
+        var predicates = new ArrayList<Predicate>();
 
         var functionDateDataCriacao = builder.function(
                 "date_trunc", Date.class,
@@ -36,6 +39,20 @@ public class VendaQueryServiceImpl implements VendaQueryService {
                 builder.sum(root.get("valorTotal")));
 
         query.select(selection);
+
+        if(filtro.getRestauranteId() != null) {
+            predicates.add(builder.equal(root.get("restaurante"), filtro.getRestauranteId()));
+        }
+
+        if(filtro.getDataCriacaoInicio() != null) {
+            predicates.add(builder.greaterThanOrEqualTo(root.get("dataCriacao"), filtro.getDataCriacaoInicio()));
+        }
+
+        if(filtro.getDataCriacaoFim() != null) {
+            predicates.add(builder.lessThanOrEqualTo(root.get("dataCriacao"), filtro.getDataCriacaoFim()));
+        }
+
+        query.where(predicates.toArray(new Predicate[0]));
         query.groupBy(functionDateDataCriacao);
 
         TypedQuery<VendaDiaria> typedQuery = manager.createQuery(query);
