88f813e5a3264afc064aad35af49932cdaa29152
andrelgirao <andrelgirao22@hotmail.com>
2022-04-04 09:05:00 -0300

    Configurando o Resource Server com a nova stack do Spring Security

---
+++ src/main/java/br/com/alg/giraofoodapi/core/security/ResourceServerConfig.java
@@ -0,0 +1,19 @@
+package br.com.alg.giraofoodapi.core.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+            .and()
+                .oauth2ResourceServer().opaqueToken();
+    }
+
+}
