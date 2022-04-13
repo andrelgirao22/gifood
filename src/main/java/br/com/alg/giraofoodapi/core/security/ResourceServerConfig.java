5e064c2b948fe0261de8cd295193dbd94fb3e024
andrelgirao <andrelgirao22@hotmail.com>
2022-04-13 08:58:37 -0300

    refatorando serviços de usuários para usar BCrypt

--- src/main/java/br/com/alg/giraofoodapi/core/security/ResourceServerConfig.java
+++ src/main/java/br/com/alg/giraofoodapi/core/security/ResourceServerConfig.java
@@ -1,8 +1,13 @@
 package br.com.alg.giraofoodapi.core.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
+import javax.crypto.spec.SecretKeySpec;
 
 @EnableWebSecurity
 public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
@@ -14,7 +19,13 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
             .and().cors()
             .and()
-                .oauth2ResourceServer().opaqueToken();
+                .oauth2ResourceServer().jwt();
     }
+
+//    @Bean
+//    public JwtDecoder jwtDecoder() {
+//        var secretKey = new SecretKeySpec("89a7assd893564sdasd4as6a5sdasdasda1545ass4s".getBytes(), "HmacSHA256");
+//        return NimbusJwtDecoder.withSecretKey(secretKey).build();
+//    }
 
 }
--- src/main/java/br/com/alg/giraofoodapi/domain/service/CadastroUsuarioService.java
+++ src/main/java/br/com/alg/giraofoodapi/domain/service/CadastroUsuarioService.java
@@ -9,6 +9,7 @@ import br.com.alg.giraofoodapi.domain.model.Usuario;
 import br.com.alg.giraofoodapi.domain.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,6 +27,8 @@ public class CadastroUsuarioService {
     @Autowired
     private CadastrosRestauranteService restauranteService;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Transactional
     public Usuario salvar(Usuario usuario) {
@@ -34,6 +37,9 @@ public class CadastroUsuarioService {
         Optional<Usuario> usuarioExistente = repository.findByEmail(usuario.getEmail());
         if(usuarioExistente.isPresent() && !usuarioExistente.get().equals(usuario)) {
             throw new NegocioException(String.format("Já existe um usuário cadastrado com o email: %s", usuario.getEmail()));
+        }
+        if(usuario.isNovo()) {
+            usuario.setSenha(passwordEncoder.encode(usuario.getSenha()));
         }
 
         return repository.save(usuario);
@@ -46,10 +52,10 @@ public class CadastroUsuarioService {
     @Transactional
     public void alterarSenha(Long id, String senhaAtual, String novaSenha) {
         Usuario usuario = buscar(id);
-        if(usuario.senhaNaoCoincideCom(senhaAtual)) {
+        if(!passwordEncoder.matches(senhaAtual, usuario.getSenha())) {
             throw new NegocioException("Senha atual informada não coincide com a senha do usuário.");
         }
-        usuario.setSenha(novaSenha);
+        usuario.setSenha(passwordEncoder.encode(novaSenha));
     }
 
     @Transactional
